3e9d20dfa648974ffd0a2bbad2fb54f7c340fcd0
hi3816 <wlgp2534@naver.com>
2024-11-26 19:54:30 +0900

    [Refactor] UI관련 폴더 생성

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : SingletonBase<UIManager>
+{
+    public void Show()
+    { 
+    }
+
+    private void Load()
+    {
+    }
+
+    public void Hide()
+    {
+    }
+
+}
