920631a3489b5e9829fc5191fff7363f1913fddd
o.o <6whiteowl6@gmail.com>
2024-12-03 20:40:40 +0900

    [Update] BT 구조 수정
    - BT 구조 수정(노드 재배치 및 삭제)
    - 로직 작동하는지 단위 테스트 완료

--- Assets/Scripts/Humans/HumanTest.cs
+++ Assets/Scripts/Humans/HumanTest.cs
@@ -5,19 +5,35 @@ public class HumanTest : MonoBehaviour
     private Human _human;
     [SerializeField] private Human humanPrefab;
     [SerializeField] private Transform spawnPoint;
+    private HumanController _humanController;
     
-    private void Awake()
+    private void Start()
     {
         _human = Instantiate(humanPrefab, spawnPoint.position, Quaternion.identity); // 스폰 위치에 생성
+        _humanController = _human.GetComponent<HumanController>();
     }
 
     private void Update()
     {
-        // TestCode
+        #region TestCode
+
         // 스페이스바 누르면 웨이브 시작
         if (Input.GetKeyDown("space"))
         {
             _human.IsWaveStarted = true;
         }
+
+        // A키 누르면 몬스터에게 인간이 공격을 당한 것(깜짝 놀란 것)으로 간주
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _human.targetMonster = FindObjectOfType<Monster>();
+            if (_human.targetMonster == null)
+                Debug.LogAssertion("TargetMonster is null");
+            // 몬스터의 시작 피로도 임의로 설정(디폴트 값도 0임)
+            _human.targetMonster.CurrentFatigue = 0;
+            _humanController.ReactToScaring();
+        }
+
+        #endregion
     }
 }
