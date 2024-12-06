442f6c51969d1e9d298e26579500f69e58743a55
hi3816 <wlgp2534@naver.com>
2024-12-06 10:58:45 +0900

    [fix] 로비 씬 스크립트 클래스명 수정

--- Assets/Scripts/UI/LobbyScene.cs
+++ Assets/Scripts/UI/LobbyScene.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LobbyScreen : MonoBehaviour
+public class LobbyScene : MonoBehaviour
 {
     private void Awake()
     {
