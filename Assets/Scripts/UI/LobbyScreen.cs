8a35148ff09108687bd7c0f6095b4ba7efc96eda
hi3816 <wlgp2534@naver.com>
2024-12-06 10:47:01 +0900

    [ADD] 로비 관리 스크립트 추가

---
+++ Assets/Scripts/UI/LobbyScreen.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyScreen : MonoBehaviour
+{
+    private void Awake()
+    {
+        //로비 시작부분
+    }
+}
