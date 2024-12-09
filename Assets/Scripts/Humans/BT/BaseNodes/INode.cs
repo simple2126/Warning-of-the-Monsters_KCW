325252ffae6b98d93f9774e507c17904f277af9c
o.o <6whiteowl6@gmail.com>
2024-12-09 09:45:29 +0900

    [Remove] 사용하지 않는 파일 정리

--- Assets/Scripts/Humans/BT/BaseNodes/INode.cs
+++
@@ -1,7 +0,0 @@
-public enum NodeState { Success, Failure, Running }
-
-public interface INode
-{
-    // 현재 노드 어떤 상태인지 반환
-    NodeState Evaluate();
-}
---
+++ Assets/Scripts/Managers/HumanManager.cs
@@ -0,0 +1,4 @@
+public class HumanManager :SingletonBase<HumanManager>
+{
+
+}
