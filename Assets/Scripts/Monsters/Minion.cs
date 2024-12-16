eb26ee4d88b3e50f7b47e61889ff0aa676d238d3
Eunseol Park <kongpark2019@gmail.com>
2024-12-17 05:13:59 +0900

    [Update]Minion

--- Assets/Scripts/Monsters/Minion.cs
+++ Assets/Scripts/Monsters/Minion.cs
@@ -5,7 +5,24 @@ public class Minion : Monster //졸개
 {
     private NavMeshAgent _navMeshAgent;
     private Monster_Data.Monster_Data _minionData;
+    private summonerMonster _summonerMonster;
 
+    public void InitializeMinion(Monster_Data.Monster_Data minionData)
+    {
+        _minionData = minionData;
+        data.fatigue = minionData.fatigue;
+        data.minFearInflicted = minionData.minFearInflicted;
+        data.maxFearInflicted = minionData.maxFearInflicted;
+        data.cooldown = minionData.cooldown;
+        data.humanDetectRange = minionData.humanDetectRange;
+        data.humanScaringRange = minionData.humanScaringRange;
+        data.walkSpeed = minionData.walkspeed;
+        
+        _navMeshAgent.speed = minionData.walkspeed;
+        Animator.speed = _navMeshAgent.speed;
+        SetState(MonsterState.Idle);
+    }
+    
     protected override void Awake()
     {
         base.Awake();
@@ -20,30 +37,13 @@ public class Minion : Monster //졸개
         switch (MonsterState)
         {
          case MonsterState.Idle:
-             
+             HandleWalking();
              break;
          case MonsterState.Walking:
-             
              break;
         }
     }
     
-    public void InitializeMinion(Monster_Data.Monster_Data minionData)
-    {
-        _minionData = minionData;
-        data.id = minionData.id;
-        data.fatigue = minionData.fatigue;
-        data.minFearInflicted = minionData.minFearInflicted;
-        data.maxFearInflicted = minionData.maxFearInflicted;
-        data.cooldown = minionData.cooldown;
-        data.humanDetectRange = minionData.humanDetectRange;
-        data.humanScaringRange = minionData.humanScaringRange;
-        data.walkSpeed = minionData.walkspeed;
-        data.maxLevel = minionData.maxLevel;
-        _navMeshAgent.speed = minionData.walkspeed;
-        SetState(MonsterState.Walking);
-    }
-
     protected override void SetState(MonsterState state)
     {
         base.SetState(state);
@@ -55,12 +55,28 @@ public class Minion : Monster //졸개
         }
     }
     
-    private void WalkTowardsNearestHuman()
+    private void HandleWalking()
     {
         Transform nearestHuman = GetNearestHuman();
+        float distanceToSummoner = Vector3.Distance(transform.position, _summonerMonster.transform.position);
         if (nearestHuman != null)
         {
-            _navMeshAgent.SetDestination(nearestHuman.position);
+            float distanceToHuman = Vector3.Distance(transform.position, nearestHuman.position);
+            if (distanceToHuman <= data.humanDetectRange)
+            {
+                _navMeshAgent.SetDestination(nearestHuman.position);
+                SetState(MonsterState.Walking);
+                if (distanceToHuman <= data.humanScaringRange)
+                {
+                    SetState(MonsterState.Scaring);
+                }
+                
+                if (distanceToSummoner > 5f) // 만약 미니언이 소환사와 너무 멀어지면
+                {
+                    _navMeshAgent.SetDestination(_summonerMonster.transform.position);
+                    SetState(MonsterState.Walking);
+                }
+            } 
         }
         else
         {
