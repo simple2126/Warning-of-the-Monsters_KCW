2966a8a37fa6334e1d4bf937462c8dd5b6c39fae
simple2126 <kimcw212@gmail.com>
2024-12-19 11:48:39 +0900

    [Fix] MonsterEvolution
    메두사 진화 데이터는 있으나 아직 진화 몬스터 만들지 않아 최대 레벨 비교해서 제외시킴

--- Assets/Scripts/Monsters/MonsterUpgrade.cs
+++ Assets/Scripts/Monsters/MonsterUpgrade.cs
@@ -35,7 +35,8 @@ public class MonsterUpgrade : MonoBehaviour
     {
         if (clickedMonster.data.currentLevel <= clickedMonster.data.maxLevel)
         {
-            if (MonsterDataManager.Instance.GetEvolutionData(clickedMonster.data.id, clickedMonster.data.currentLevel + 1) != null)
+            Monster_Data.Evolution_Data data = MonsterDataManager.Instance.GetEvolutionData(clickedMonster.data.id, clickedMonster.data.currentLevel + 1);
+            if (data != null && data.upgrade_level == clickedMonster.data.maxLevel)
             {
                 evolutionUI.Show(clickedMonster);
             }
