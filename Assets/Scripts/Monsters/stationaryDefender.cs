590b26b16bdcf8a70f017492757698c6603c94f4
Eunseol Park <kongpark2019@gmail.com>
2025-01-03 16:13:52 +0900

    [Add]PackageManager

--- Assets/Scripts/Monsters/stationaryDefender.cs
+++ Assets/Scripts/Monsters/stationaryDefender.cs
@@ -10,26 +10,26 @@ public class stationaryDefender : Monster //한자리를 지키고 있는 몬스
         base.Update();
     }
 
-    protected override void Scaring(float time)
-    {
-        base.Scaring(time);
-       
-        // 범위 공격
-        // if (Time.time - _lastScareTime > data.cooldown)
-        // {
-        //     foreach (Human human in _targetHumanList)
-        //     {
-        //         if (human == null) continue;
-        //
-        //         human.IncreaseFear(data.fearInflicted);
-        //         //human.controller.SetTargetMonster(transform);
-        //     }
-        //     _lastScareTime = Time.time;
-        // }
-
-        if (TargetHumanList.Count == 0)
-        {
-            SetState(MonsterState.Idle);
-        }
-    }
+    // protected override void Scaring(float time)
+    // {
+    //     base.Scaring(time);
+    //    
+    //     // 범위 공격
+    //     // if (Time.time - _lastScareTime > data.cooldown)
+    //     // {
+    //     //     foreach (Human human in _targetHumanList)
+    //     //     {
+    //     //         if (human == null) continue;
+    //     //
+    //     //         human.IncreaseFear(data.fearInflicted);
+    //     //         //human.controller.SetTargetMonster(transform);
+    //     //     }
+    //     //     _lastScareTime = Time.time;
+    //     // }
+    //
+    //     if (TargetHumanList.Count == 0)
+    //     {
+    //         SetState(MonsterState.Idle);
+    //     }
+    // }
 }
