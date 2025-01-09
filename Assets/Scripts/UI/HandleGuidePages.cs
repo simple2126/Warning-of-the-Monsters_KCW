3656b181a8ec216fd5cd4009595647044e6fb56f
Eunseol Park <kongpark2019@gmail.com>
2025-01-09 15:36:22 +0900

    [Update]Guide
    가이드 페이지 4 추가

--- Assets/Scripts/UI/HandleGuidePages.cs
+++ Assets/Scripts/UI/HandleGuidePages.cs
@@ -10,7 +10,9 @@ public class HandleGuidePages : UIBase
     [SerializeField] private Button _bookmark1Button;
     [SerializeField] private Button _bookmark2LeftButton;
     [SerializeField] private Button _bookmark2RightButton;
-    [SerializeField] private Button _bookmark3Button;
+    [SerializeField] private Button _bookmark3LeftButton;
+    [SerializeField] private Button _bookmark3RightButton;
+    [SerializeField] private Button _bookmark4Button;
 
     private int _currentPage = 0;
 
@@ -19,7 +21,9 @@ public class HandleGuidePages : UIBase
         _bookmark1Button.onClick.AddListener(() => ShowPage(0));
         _bookmark2LeftButton.onClick.AddListener(() => ShowPage(1));
         _bookmark2RightButton.onClick.AddListener(() => ShowPage(1));
-        _bookmark3Button.onClick.AddListener(() => ShowPage(2));
+        _bookmark3LeftButton.onClick.AddListener(() => ShowPage(2));
+        _bookmark3RightButton.onClick.AddListener(() => ShowPage(2));
+        _bookmark4Button.onClick.AddListener(() => ShowPage(3));
         ShowPage(0);
     }
 
@@ -36,19 +40,32 @@ public class HandleGuidePages : UIBase
 
     private void UpdateButtonVisibility()
     {
-        if (_currentPage == 2)
+        if (_currentPage == 2) //page3
         {
             _bookmark1Button.gameObject.SetActive(true);
             _bookmark2LeftButton.gameObject.SetActive(true);
             _bookmark2RightButton.gameObject.SetActive(false);
-            _bookmark3Button.gameObject.SetActive(true);
+            _bookmark3LeftButton.gameObject.SetActive(false);
+            _bookmark3RightButton.gameObject.SetActive(true);
+            _bookmark4Button.gameObject.SetActive(true);
         }
-        else
+        else if (_currentPage == 3) //page4
+        {
+            _bookmark1Button.gameObject.SetActive(true);
+            _bookmark2LeftButton.gameObject.SetActive(true);
+            _bookmark2RightButton.gameObject.SetActive(false);
+            _bookmark3LeftButton.gameObject.SetActive(true);
+            _bookmark3RightButton.gameObject.SetActive(false);
+            _bookmark4Button.gameObject.SetActive(true);
+        }
+        else //page 1, 2
         {
             _bookmark1Button.gameObject.SetActive(true);
             _bookmark2LeftButton.gameObject.SetActive(false);
             _bookmark2RightButton.gameObject.SetActive(true);
-            _bookmark3Button.gameObject.SetActive(true);
+            _bookmark3LeftButton.gameObject.SetActive(false);
+            _bookmark3RightButton.gameObject.SetActive(true);
+            _bookmark4Button.gameObject.SetActive(true);
         }
     }
 }
