b69899d765e080a07455c83c55cfa611c5086b82
simple2126 <kimcw212@gmail.com>
2025-01-09 09:28:26 +0900

    [Chore] Tesst_EndWaveButton
    밸런스 조절을 위해 안 쓰는 코드 및 오브젝트 제거

--- Assets/Scripts/Managers/StageManager.cs
+++ Assets/Scripts/Managers/StageManager.cs
@@ -55,7 +55,6 @@ public class StageManager : SingletonBase<StageManager>
         CurrWave = 0;
         CurrHealth = StageData.health;
         CurrGold = StageData.gold;
-        CurrGold = 10000;
         StarsCount = 0;
         _stageInfoController = _stageInfo.GetComponent<StageInfoController>();
         _stageInfoController.ChangeUI();
