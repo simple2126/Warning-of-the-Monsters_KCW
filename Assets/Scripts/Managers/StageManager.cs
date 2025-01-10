ebcd5609048961346a8e469be35119e74af9090a
Eunseol Park <kongpark2019@gmail.com>
2025-01-10 11:10:15 +0900

    [Update]StageManager.cs

--- Assets/Scripts/Managers/StageManager.cs
+++ Assets/Scripts/Managers/StageManager.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class StageManager : SingletonBase<StageManager>
@@ -39,6 +41,10 @@ public class StageManager : SingletonBase<StageManager>
     {
         Time.timeScale = 1;
         GameManager.Instance.isPlaying = true;
+    }
+
+    private void Start()
+    {
         PoolManager.Instance.AddPools<SfxSoundSource>(_poolConfigs);
         SoundManager.Instance.PlayBGM(BgmType.Stage);
         SetStageInfo();
