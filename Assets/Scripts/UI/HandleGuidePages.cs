c4b13909cbfda696cb6da5ead384416daa50a4c0
Eunseol Park <kongpark2019@gmail.com>
2025-01-15 18:22:48 +0900

    [Update] GuidePanel
    Summoner 몬스터의 미니언 위치 지정 기능 설명 페이지 추가

--- Assets/Scripts/UI/HandleGuidePages.cs
+++ Assets/Scripts/UI/HandleGuidePages.cs
@@ -7,7 +7,8 @@ public class HandleGuidePages : UIBase
     [SerializeField] private GameObject[] pages;
 
     [Header("Buttons")]
-    [SerializeField] private Button _bookmark1Button;
+    [SerializeField] private Button _bookmark1LeftButton;
+    [SerializeField] private Button _bookmark1RightButton;
     [SerializeField] private Button _bookmark2LeftButton;
     [SerializeField] private Button _bookmark2RightButton;
     [SerializeField] private Button _bookmark3LeftButton;
@@ -18,7 +19,8 @@ public class HandleGuidePages : UIBase
 
     void Start()
     {
-        _bookmark1Button.onClick.AddListener(() => ShowPage(0));
+        _bookmark1LeftButton.onClick.AddListener(() => ShowPage(0));
+        _bookmark1RightButton.onClick.AddListener(() => ShowPage(0));
         _bookmark2LeftButton.onClick.AddListener(() => ShowPage(1));
         _bookmark2RightButton.onClick.AddListener(() => ShowPage(1));
         _bookmark3LeftButton.onClick.AddListener(() => ShowPage(2));
@@ -40,31 +42,44 @@ public class HandleGuidePages : UIBase
 
     private void UpdateButtonVisibility()
     {
-        if (_currentPage == 2) //page3
+        if (_currentPage == 0) //page 1
         {
-            _bookmark1Button.gameObject.SetActive(true);
-            _bookmark2LeftButton.gameObject.SetActive(true);
-            _bookmark2RightButton.gameObject.SetActive(false);
-            _bookmark3LeftButton.gameObject.SetActive(false);
-            _bookmark3RightButton.gameObject.SetActive(true);
-            _bookmark4Button.gameObject.SetActive(true);
-        }
-        else if (_currentPage == 3) //page4
-        {
-            _bookmark1Button.gameObject.SetActive(true);
-            _bookmark2LeftButton.gameObject.SetActive(true);
-            _bookmark2RightButton.gameObject.SetActive(false);
-            _bookmark3LeftButton.gameObject.SetActive(true);
-            _bookmark3RightButton.gameObject.SetActive(false);
-            _bookmark4Button.gameObject.SetActive(true);
-        }
-        else //page 1, 2
-        {
-            _bookmark1Button.gameObject.SetActive(true);
+            _bookmark1LeftButton.gameObject.SetActive(false);
+            _bookmark1RightButton.gameObject.SetActive(true);
             _bookmark2LeftButton.gameObject.SetActive(false);
             _bookmark2RightButton.gameObject.SetActive(true);
             _bookmark3LeftButton.gameObject.SetActive(false);
             _bookmark3RightButton.gameObject.SetActive(true);
+            _bookmark4Button.gameObject.SetActive(true);
+        }
+        else if (_currentPage == 1) //page2
+        {
+            _bookmark1LeftButton.gameObject.SetActive(true);
+            _bookmark1RightButton.gameObject.SetActive(false);
+            _bookmark2LeftButton.gameObject.SetActive(false);
+            _bookmark2RightButton.gameObject.SetActive(true);
+            _bookmark3LeftButton.gameObject.SetActive(false);
+            _bookmark3RightButton.gameObject.SetActive(true);
+            _bookmark4Button.gameObject.SetActive(true);
+        }
+        else if (_currentPage == 2) //page3
+        {
+            _bookmark1LeftButton.gameObject.SetActive(true);
+            _bookmark1RightButton.gameObject.SetActive(false);
+            _bookmark2LeftButton.gameObject.SetActive(true);
+            _bookmark2RightButton.gameObject.SetActive(false);
+            _bookmark3LeftButton.gameObject.SetActive(false);
+            _bookmark3RightButton.gameObject.SetActive(true);
+            _bookmark4Button.gameObject.SetActive(true);
+        }
+        else //page4
+        {
+            _bookmark1LeftButton.gameObject.SetActive(true);
+            _bookmark1RightButton.gameObject.SetActive(false);
+            _bookmark2LeftButton.gameObject.SetActive(true);
+            _bookmark2RightButton.gameObject.SetActive(false);
+            _bookmark3LeftButton.gameObject.SetActive(true);
+            _bookmark3RightButton.gameObject.SetActive(false);
             _bookmark4Button.gameObject.SetActive(true);
         }
     }
